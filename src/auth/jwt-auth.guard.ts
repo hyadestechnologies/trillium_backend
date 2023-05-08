2ae80a0a6cbdb96da20ec563da0b2e3e278638f1
JustCris <cristian.scapin654@icloud.com>
2023-05-08 09:31:22 +0200

    feat: Optional() flag in routes for auth

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -2,7 +2,10 @@ import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
-import { IS_PUBLIC_KEY } from 'src/decorators/public-decorator';
+import {
+  IS_PUBLIC_KEY,
+  IS_OPTIONAL_KEY,
+} from 'src/decorators/public-decorator';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -22,5 +25,19 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
       return true;
     }
     return super.canActivate(context);
+  }
+
+  handleRequest(err, user, info, context) {
+    const isOptional = this.reflector.getAllAndOverride<boolean>(
+      IS_OPTIONAL_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    // if the api is optional (declared with `@Optional`) req.user will be valorized with
+    // the user id and username if authentication token is provided, false otherwise
+    if (isOptional) {
+      return user;
+    }
+    return super.handleRequest(err, user, info, context);
   }
 }
